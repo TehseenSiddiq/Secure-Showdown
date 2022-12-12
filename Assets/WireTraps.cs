2f9e46de3bcc1a8c1c97451ce10ebc04fc691a1a
SpankyBoy <59478102+TehseenSiddiq@users.noreply.github.com>
2022-12-12 20:25:07 +0500

    WireTrap

--- Assets/WireTraps.cs
+++ Assets/WireTraps.cs
@@ -6,27 +6,32 @@ public class WireTraps : MonoBehaviour
 {
     public Transform[] points;
     [SerializeField] LineRenderer lr;
-    public Gradient gradient;
+    public Gradient caughtColor, warningColor,workingColor;
+    public float range = 5;
     public void Update()
     {
-      
-            lr.SetPosition(0, points[0].position);
-            lr.SetPosition(1, points[1].position);
+        lr.SetPosition(0, points[0].position);
+        lr.SetPosition(1, points[1].position);
 
         Vector2 dir = (points[0].position - points[1].position).normalized;
 
         RaycastHit2D hit = Physics2D.Raycast(points[0].position, -dir, Vector2.Distance(points[0].position, points[1].position));
         Debug.Log(hit.collider);
+        if(Vector2.Distance(points[0].position,points[1].position) > range)
+        {
+            lr.colorGradient = warningColor;
+            return;
+        }
         if (hit.collider != null)
         {
-              lr.SetColors(Color.red, Color.red);
-              Debug.DrawLine(points[0].position, hit.point, Color.magenta);
-              Debug.DrawRay(points[1].position, -dir, Color.red);
-
+            lr.colorGradient = workingColor;
+            Debug.DrawLine(points[0].position, hit.point, Color.magenta);
+            Debug.DrawRay(points[1].position, -dir, Color.red);
         }
+        
         else
         {
-              lr.colorGradient = gradient;
-         }
+              lr.colorGradient = caughtColor;
+        }
     }
 }
